6f50994e59ea54fae20fcf12bff4318f298342c1
kotecku <krystowskipiotr2005@gmail.com>
2025-03-09 23:59:10 +0100

    Added movestack patch

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
+#include "movestack.c"
 /* CONFIG BY UWU KOTICK_XXX_2016 */
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -80,6 +81,8 @@ static const Key keys[] = {
 	{ MODKEY,			                  XK_s,	     spawn,	         {.v = pavucntrl } },
 	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_k,      setmfact,       {.f = +0.05} },
+  { MODKEY|ShiftMask,             XK_g,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_h,      movestack,      {.i = -1 } },
 	{ MODKEY,             		      XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
